3ca11265837cedfe60288255f351277010a1ef36
Sagar Shah <sagarshah2326@gmail.com>
2017-09-14 11:33:25 +0000

    Add mka command
    Change-Id: If9da6c2edf6a664f203a487571d68db8e9f70674
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>
    
    envsetup: Update mka
    
    Change-Id: I36ec10be9152f135ec3645b4304e63ea30d06774
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -20,6 +20,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep:   Greps on all local sepolicy files.
 - sgrep:     Greps on all local source files.
 - godir:     Go to the directory containing a file.
+- mka:       Builds using SCHED_BATCH on all processors
 
 Environment options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -1538,6 +1539,17 @@ function godir () {
         pathname=${lines[0]}
     fi
     \cd $T/$pathname
+}
+
+function mka() {
+    case `uname -s` in
+        Darwin)
+            m -j "$@"
+            ;;
+        *)
+            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
+            ;;
+    esac
 }
 
 # Force JAVA_HOME to point to java 1.7/1.8 if it isn't already set.
